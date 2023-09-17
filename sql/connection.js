bce63cdbf4ac892180826076791e9d61db04f34a
llthompson <lisalacythompson+github@gmail.com>
2023-09-17 10:58:53 -0500

    setup

--- sql/connection.js
+++ sql/connection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql')
+const mysql2 = require('mysql2')
 
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating connection...')
-      this.pool = mysql.createPool({
+      this.pool = mysql2.createPool({
         connectionLimit: 100,
         host: 'localhost',
         user: 'root',
